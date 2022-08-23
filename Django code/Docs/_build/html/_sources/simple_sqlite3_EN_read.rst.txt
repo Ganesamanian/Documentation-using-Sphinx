9bda8f08791d9c969fab9451ade87ec65ed57820
Ganesamanian Kolappan <38348456+Ganesamanian@users.noreply.github.com>
2022-08-23 22:34:22 +0200

    Deleting the directory

--- Django code/Docs/_build/html/_sources/simple_sqlite3_EN_read.rst.txt
+++
@@ -1,7 +0,0 @@
-simple\_sqlite3\_EN\_read module
-================================
-
-.. automodule:: simple_sqlite3_EN_read
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- Django code/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='Myapp-Home'),
-    path('about/', views.about, name='Myapp-about'),
-    
-]
